0995949089395999ee05b15203b709b10a4d49d7
Armando Gioielli <armando.gioielli@linksmt.it>
2025-10-12 21:26:57 +0200

    Modifica script 1_DDL Struttura Database

--- DDL/0_DDL_Create Database.sql
+++
@@ -1,8 +0,0 @@
--- ============================================================
---  Creazione Database ferrovia con utenza ferrovia_user_admin
--- ============================================================
-
-CREATE DATABASE ferrovia WITH TEMPLATE = template0   ENCODING 'UTF8'  LC_COLLATE = 'en_US.utf8'  LC_CTYPE = 'en_US.utf8';
-CREATE USER ferrovia_user_admin WITH PASSWORD 'ferrovia_user_admin_psw!';
-ALTER DATABASE ferrovia OWNER TO ferrovia_user_admin;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO ferrovia_user_admin;
